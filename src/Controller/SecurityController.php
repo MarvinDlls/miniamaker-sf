83cb874905128e259cdcebb1fcc265c5b1158d17
Marvindlls <marvindillenseger@gmail.com>
2025-02-04 16:23:54 +0100

    Add LoginHistory entity and repository; enhance login method to track user login details

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\LoginHistory;
+use DeviceDetector\DeviceDetector;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,8 +13,30 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(
+        AuthenticationUtils $authenticationUtils,
+        Request $request,
+        // DeviceDetector $deviceDetector
+        ): Response
     {
+        $userAgent = $request->headers->get('User-Agent');
+        $deviceDetector = new DeviceDetector($userAgent);
+        $deviceDetector->parse();
+        $device = $deviceDetector->getDeviceName();
+        $os = $deviceDetector->getOs();
+        $browser = $deviceDetector->getClient();
+
+        if ($this->getUser()) {
+            $loginHistory = new LoginHistory();
+            $loginHistory
+                ->setUser($this->getUser())
+                ->setIpAddress($request->getClientIp())
+                ->setDevice($device)
+                ->setOs($os['name'])
+                ->setBrowser($browser['name'])
+                ;
+            dd($loginHistory);
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
