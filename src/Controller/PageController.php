33c1a3bbef598e76ad415a2cbfe25b4965f8b0b7
Marvindlls <marvindillenseger@gmail.com>
2025-02-05 10:14:53 +0100

    Implement LoginHistoryService to track user login details and update homepage logic to log user access

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,15 +2,31 @@
 
 namespace App\Controller;
 
+use App\Service\LoginHistoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request, LoginHistoryService $lhs): Response
     {
+        if (
+                $this->getUser() && // Si l'utilisateur est connecté
+                $request->headers->get('referer') == 'https://127.0.0.1:8000/login'
+            ) 
+        {
+            $lhs->addHistory(
+                $this->getUser(),
+                $request->headers->get('user-agent'),
+                $request->getClientIp()
+            );
+        }
+
+        dd($request);
+
         if (!$this->getUser()) {
             return $this->render('page/lp.html.twig');
         }
---
+++ src/Service/LoginHistoryService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Entity\LoginHistory;
+use DeviceDetector\DeviceDetector;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * Classe de gestion de l'historique de l'utilisateur
+ */
+
+class LoginHistoryService
+{
+    public function __construct(readonly private EntityManagerInterface $em){}
+
+    public function addHistory(User $user, string $userAgent, string $ip): void
+    {
+        $deviceDetector = new DeviceDetector('userAgent');
+        $deviceDetector->parse();
+
+        if ($user) {
+            $loginHistory = new LoginHistory();
+            $loginHistory
+                ->setUser($user)
+                ->setIpAddress($ip)
+                ->setDevice($deviceDetector->getDeviceName())
+                ->setOs($deviceDetector->getOs()['name'])
+                ->setBrowser($deviceDetector->getClient()['name'])
+                ;
+            $this->em->persist($loginHistory);
+            $this->em->flush();
+        }
+    }
+}
