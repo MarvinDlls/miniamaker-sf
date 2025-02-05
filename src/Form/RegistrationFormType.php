d021de15b243cce7d7fc2b1ad7d7ccbb4ef797ed
Marvindlls <marvindillenseger@gmail.com>
2025-02-05 14:42:54 +0100

    Refactor registration form labels and messages for clarity and consistency

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,39 +4,39 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType ::class, [
-                'row_attr' => ['class' => 'mb-3' ],
-                'label' => 'Votre adresse e-amil',
+            ->add('email', EmailType::class, [
+                'row_attr' => ['class' => 'mb-3'],
+                'label' => 'Votre adresse e-mail',
                 'label_attr' => [
                     'class' => 'form-label',
                 ],
                 'attr' => [
-                    'placeholder' => 'exemple@exemple.com',
+                    'placeholder' => 'martin@gmail.com',
                     'class' => 'form-control',
-                ]
-            ])
+                ],
+            ]) 
             ->add('plainPassword', RepeatedType::class, [
-                'row_attr' => ['class' => 'mb-3' ],
+                'row_attr' => ['class' => 'mb-3'],
                 'label' => 'Mot de passe',
-                'label_attr' => ['class' => 'form-label'],
-                'type' => PasswordType::class, // avec quoi tu es associé à la répétition
-                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'label_attr' => [ 'class' => 'form-label'],
+                'type' => PasswordType::class, //  avec quoi tu es associé à la répétition
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
                 'mapped' => false,
                 'first_options' => [
                     'label_attr' => ['class' => 'form-label'],
@@ -50,7 +50,7 @@ class RegistrationFormType extends AbstractType
                 ],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Merci de saisir un mot de passe.',
+                        'message' => 'Merci de saisir un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
@@ -61,44 +61,44 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('isMinor', CheckboxType::class, [
-                'row_attr' => ['class' => 'form-check'],
+                'row_attr' => ['class' => 'form-check mb-2'],
                 'label_attr' => ['class' => 'form-check-label'],
                 'attr' => ['class' => 'form-check-input'],
-                'label' => "Vous confirmez que vous êtes majeur.",
+                'label' => "Vous confirmez que vous êtes majeur",
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'Vous devez être majeur pour vous inscrire.',
+                        'message' => 'Vous devez être majeur pour vous inscrire',
                     ]),
                 ],
             ])
             ->add('isTerms', CheckboxType::class, [
-                'row_attr' => ['class' => 'form-check'],
+                'row_attr' => ['class' => 'form-check mb-2'],
                 'label_attr' => ['class' => 'form-check-label'],
                 'attr' => ['class' => 'form-check-input'],
-                'label' => "Vous confirmez que vous avez lu les CGU.",
+                'label' => "J'accepte les CGU et CGV",
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'Vous devez accepter les CGU pour vous inscrire.',
+                        'message' => 'Vous devez accepter les CGU pour vous inscrire',
                     ]),
                 ],
             ])
             ->add('isGpdr', CheckboxType::class, [
-                'row_attr' => ['class' => 'form-check'],
+                'row_attr' => ['class' => 'form-check mb-2'],
                 'label_attr' => ['class' => 'form-check-label'],
                 'attr' => ['class' => 'form-check-input'],
-                'label' => "Vous confirmez que vous avez lu notre politique RGPD.",
+                'label' => "J'accepte la politique RGPD de miniamaker",
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'Vous devez accepter notre politique RGPD pour vous inscrire.',
+                        'message' => 'Vous devez notre politique RGPD pour vous inscrire',
                     ]),
                 ],
             ])
-            ->add('submit', SubmitType ::class, [
-                'label' => 'S\'inscrire',
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
                 'attr' => [
                     'class' => 'btn btn-primary',
-                ]
-            ])
+                ],
+            ]) 
         ;
     }
 
