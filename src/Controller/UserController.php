b9c46764a1240bbc27ba8536b139f5def3d7658d
Marvindlls <marvindillenseger@gmail.com>
2025-02-05 15:47:52 +0100

    Enhance user login history tracking and add profile completion route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +15,20 @@ final class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    #[Route('/complete', name: 'app_complete', methods: ['POST'])]
+    public function complete(Request $request): Response
+    {
+        $username = $request->request->get('username');
+        $fullname = $request->request->get('fullname');
+
+        if (!empty($username) && !empty($fullname)) {
+
+        }
+
+        // Redirect avec flash message
+        $this->addFlash('success', 'Votre profile est complété');
+        return $this->redirectToRoute('app_profile');
     }
 }
