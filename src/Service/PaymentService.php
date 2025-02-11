aac45781211b541048a59fe267514f4e0eb598d6
Marvindlls <marvindillenseger@gmail.com>
2025-02-11 16:12:23 +0100

    Refactor PaymentService to integrate Stripe Checkout for subscription payments and add error handling

--- src/Service/PaymentService.php
+++ src/Service/PaymentService.php
@@ -2,29 +2,34 @@
 
 namespace App\Service;
 
-use App\Entity\Subscription;
+use Stripe\Price;
+use Stripe\Stripe;
 use App\Entity\User;
-use App\Repository\SubscriptionRepository;
+use App\Entity\Subscription;
+use Stripe\Checkout\Session;
 use App\Service\AbstractService;
 use Doctrine\ORM\EntityManagerInterface;
-use Stripe\Stripe;
+use App\Repository\SubscriptionRepository;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 /*
  * Classe PaymentService dédiée à la gestion du 
  * paiement des abonnements des utilisateurs
 */
+
 class PaymentService extends AbstractService
 {
     public function __construct(
         private ParameterBagInterface $params,
         private SubscriptionRepository $sr,
         private EntityManagerInterface $em,
-    ) 
-    {
+        private HttpClientInterface $httpClient,
+    ) {
         $this->params = $params;
         $this->sr = $sr;
         $this->em = $em;
+        $this->httpClient = $httpClient;
     }
 
     public function setPayment(User $user, int $amount): void
@@ -37,6 +42,22 @@ class PaymentService extends AbstractService
             ->setAmount($amount)
             ->setFrequency($amount > 99 ? $this->params->get('STRIPE_SUB_ANNUALLY') : $this->params->get('STRIPE_SUB_MONTHLY'))
         ;
-        dd($subscription);
+
+        try {
+            $checkout_session = Session::create([
+                'line_items' => [[
+                    'price' => $subscription->getFrequency(),
+                    'quantity' => 1,
+                ]],
+                'mode' => 'subscription',
+                'success_url' => $this->params->get('APP_URL') . '/subscription/success?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => $this->params->get('APP_URL') . '/subscription/cancel',
+            ]);
+
+            $this->httpClient->request('GET', $checkout_session->url);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+        // dd($subscription);
     }
 }
