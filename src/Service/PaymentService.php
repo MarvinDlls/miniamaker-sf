c0692e6d3f18e6030717ed928de0d58343c9a0fd
Marvindlls <marvindillenseger@gmail.com>
2025-02-11 12:36:43 +0100

    Implement PaymentService class with payment handling and subscription management

--- src/Service/PaymentService.php
+++ src/Service/PaymentService.php
@@ -2,10 +2,41 @@
 
 namespace App\Service;
 
-/**
- * Classe PaymentService dédiée à la gestion du paiement des abonnements des utilisateurs
-*/
-class PaymentService
-{
+use App\Entity\Subscription;
+use App\Entity\User;
+use App\Repository\SubscriptionRepository;
+use App\Service\AbstractService;
+use Doctrine\ORM\EntityManagerInterface;
+use Stripe\Stripe;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
+/*
+ * Classe PaymentService dédiée à la gestion du 
+ * paiement des abonnements des utilisateurs
+*/
+class PaymentService extends AbstractService
+{
+    public function __construct(
+        private readonly ParameterBagInterface $params,
+        private readonly SubscriptionRepository $sr,
+        private readonly EntityManagerInterface $em,
+    ) 
+    {
+        $this->params = $params;
+        $this->sr = $sr;
+        $this->em = $em;
+        // parent::__construct();
+    }
+
+    public function setPayment(User $user, int $amount): void
+    {
+        Stripe::setApiKey($this->params->get('STRIPE_SK'));
+
+        $subscription = new Subscription();
+        $subscription
+            ->addClient($user)
+            ->setAmount($amount)
+            ->setFrequency($amount > 99 ? 'annually' : 'monthly')
+        ;
+    }
 }
