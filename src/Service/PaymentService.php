cf497a57fb848bcf297a1a3895c8680e10f4d7e3
Marvindlls <marvindillenseger@gmail.com>
2025-02-12 10:16:24 +0100

    Add payment method types to Stripe Checkout session and handle missing URL case

--- src/Service/PaymentService.php
+++ src/Service/PaymentService.php
@@ -34,25 +34,26 @@ class PaymentService
 
         try {
             $checkout_session = Session::create([
+                'payment_method_types' => ['card'], // Setup du moyen de paiment
                 'line_items' => [[
                     'price_data' => [
-                        'currency' => 'eur',
-                        'unit_amount' => $amount * 100,
-                        'recurring' => [
-                            'interval' => $subscription->getFrequency(),
+                        'currency' => 'eur', // Setup de la devise
+                        'unit_amount' => $amount * 100, // Montant en centimes
+                        'recurring' => [ // recurrence de l'abonnement
+                            'interval' => $subscription->getFrequency(), // mois ou année
                         ],
-                        'product_data' => [
-                            'name' => 'Abonnement miniamaker',
+                        'product_data' => [ // Infos sur le produit
+                            'name' => 'Abonnement miniamaker', // Texte affiché sur la page de paiement
                         ],
                     ],
-                    'quantity' => 1,
+                    'quantity' => 1, // Qt obligatoire
                 ]],
                 'mode' => 'subscription',
                 'success_url' => $this->params->get('APP_URL') . '/subscription/success?session_id={CHECKOUT_SESSION_ID}',
                 'cancel_url' => $this->params->get('APP_URL') . '/subscription/cancel',
             ]);
 
-            return $checkout_session->url;
+            return $checkout_session->url ?? 'TEST';
         } catch (\Throwable $th) {
             echo $th->getMessage() . PHP_EOL;
             echo json_encode(['error' => $th->getMessage()]);
