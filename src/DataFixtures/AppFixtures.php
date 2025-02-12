1801fc826c940f4dc2b47736d3084fae3074ca2e
Marvindlls <marvindillenseger@gmail.com>
2025-02-12 12:00:17 +0100

    Add fixtures for user discussions and messages between two admin users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use App\Entity\Detail;
+use App\Entity\Discussion;
+use App\Entity\Message;
 use App\Entity\Tag;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -136,8 +138,8 @@ class AppFixtures extends Fixture
         $admin->setEmail('admin@admin.com');
         $admin->setRoles(['ROLE_ADMIN']);
         $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin123'));
-        $admin->setUsername('admin');
-        $admin->setFullname('Admin User');
+        $admin->setUsername('Martine');
+        $admin->setFullname('Admin Martine');
         $admin->setIsMajor(true);
         $admin->setIsTerms(true);
         $admin->setIsGpdr(true);
@@ -145,6 +147,43 @@ class AppFixtures extends Fixture
         
         $manager->persist($admin);
 
+        $admin2 = new User();
+        $admin2->setEmail('admin2@admin.com');
+        $admin2->setRoles(['ROLE_ADMIN']);
+        $admin2->setPassword($this->passwordHasher->hashPassword($admin, 'admin123'));
+        $admin2->setUsername('Martin');
+        $admin2->setFullname('Admin Martin');
+        $admin2->setIsMajor(true);
+        $admin2->setIsTerms(true);
+        $admin2->setIsGpdr(true);
+        $admin2->setIsVerified(true);
+        
+        $manager->persist($admin2);
+
+        // Conversion entre 2 users
+
+        $discussion = new Discussion();
+        $discussion
+            ->setSender($admin)
+            ->setReceiver($admin2)
+            ->setSubject($admin->getUsername() . ' x ' . $admin2->getUsername())
+            ->setIsArchived(false)
+            ->setCreatedAt(new \DateTimeImmutable())
+        ;
+        $manager->persist($discussion);
+
+        for ($i = 0; $i < 10; $i++) { 
+            $message = new Message();
+            $message
+                ->setDiscussion($discussion)
+                ->setUser($i % 2 === 0 ? $admin : $admin2)
+                ->setContent($faker->sentence(10))
+                ->setStatus(true)
+                ->setCreatedAt(new \DateTimeImmutable())
+            ;
+
+            $manager->persist($message);
+        }
         $manager->flush();
     }
 }
